0dcb64c249a13bbb9d8fb18202097c898c5b8ae7
twmathieu <85597990+twmathieu@users.noreply.github.com>
2021-07-02 00:08:06 -0500

    push

---
+++ Python_practice.py
@@ -0,0 +1,38 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+    if counties[3] != 'Jefferson'
+
+   print(counties[2])
+   counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")  
+counties = ["Arapahoe","Denver","Jefferson"] 
+if "El Paso" in counties:
+    print("El Paso is in the list of counties") 
+else:
+
+  counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+    if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties."
+
+for county in counties:
+    print(county)
